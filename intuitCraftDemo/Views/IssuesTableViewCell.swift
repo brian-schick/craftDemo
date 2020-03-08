465c2062648583a0ee1893fa0530922a93becda7
Brian Schick <brian_s@beezwax.net>
2020-03-08 14:45:31 -0700

    Configure issue cell in IB and set outlets to IssuesTableViewCell.swift

--- intuitCraftDemo/Views/IssuesTableViewCell.swift
+++ intuitCraftDemo/Views/IssuesTableViewCell.swift
@@ -10,5 +10,10 @@ import UIKit
 
 final public class IssuesTableViewCell: UITableViewCell {
 	
-
+	@IBOutlet weak var stateIcon: UIImageView!
+	@IBOutlet weak var titleLabel: UILabel!
+	@IBOutlet weak var numberLabel: UILabel!
+	@IBOutlet weak var openedLabel: UILabel!
+	@IBOutlet weak var labelsStack: UIStackView!
+	
 }
